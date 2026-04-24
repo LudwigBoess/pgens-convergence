2c5721537ad35d312db0b71ef0ef027e84ec23de
LudwigBoess <ludwig.boess@outlook.de>
2026-04-24 21:38:46 +0000

    simplifications

--- langmuir/pgen.hpp
+++ langmuir/pgen.hpp
@@ -21,10 +21,14 @@ namespace user {
   struct SinEDist : public arch::EnergyDistribution<S, M> {
     SinEDist(const M&                   metric,
              real_t                     v_max,
+             real_t                     temperature,
+             random_number_pool_t&      pool,
              const std::vector<int>&    n,
              const std::vector<real_t>& s)
       : arch::EnergyDistribution<S, M> { metric }
       , v_max { v_max }
+      , temperature { temperature }
+      , pool { pool }
       , kx1 { s.size() > 0 ? static_cast<real_t>(constant::TWO_PI) *
                                static_cast<real_t>(n[0]) / s[0]
                            : ZERO }
@@ -36,42 +40,41 @@ namespace user {
                            : ZERO } {}
 
     Inline void operator()(const coord_t<M::Dim>& x_Ph,
-                           vec_t<Dim::_3D>&       v,
-                           spidx_t                sp) const {
-      if (sp == 1) {
+                           vec_t<Dim::_3D>&       v) const {
+
+        // sample from a Maxwellian distribution with the given temperature, then modulate the velocity by a sinusoidal function of position
+        arch::JuttnerSinge(v, temperature, pool);
         const auto k = math::sqrt(SQR(kx1) + SQR(kx2) + SQR(kx3));
         if constexpr (M::Dim == Dim::_1D) {
-          v[0] = v_max * math::sin(kx1 * x_Ph[0]);
+          v[0] += v_max * math::sin(kx1 * x_Ph[0]);
         } else if constexpr (M::Dim == Dim::_2D) {
-          v[0] = v_max * kx1 / k * math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1]);
-          v[1] = v_max * kx2 / k * math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1]);
+          v[0] += v_max * kx1 / k * math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1]);
+          v[1] += v_max * kx2 / k * math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1]);
         } else {
-          v[0] = v_max * kx1 / k *
+          v[0] += v_max * kx1 / k *
                  math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1] + kx3 * x_Ph[2]);
-          v[1] = v_max * kx2 / k *
+          v[1] += v_max * kx2 / k *
                  math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1] + kx3 * x_Ph[2]);
-          v[2] = v_max * kx3 / k *
+          v[2] += v_max * kx3 / k *
                  math::sin(kx1 * x_Ph[0] + kx2 * x_Ph[1] + kx3 * x_Ph[2]);
         }
-      } else {
-        v[0] = ZERO;
-        v[1] = ZERO;
-        v[2] = ZERO;
-      }
     }
 
   private:
-    const real_t v_max, kx1, kx2, kx3;
+    random_number_pool_t pool;
+    const real_t v_max, temperature, kx1, kx2, kx3;
   };
 
   template <SimEngine::type S, class M>
   struct PGen : public arch::ProblemGenerator<S, M> {
 
     // compatibility traits for the problem generator
-    static constexpr auto engines = traits::compatible_with<SimEngine::SRPIC>::value;
-    static constexpr auto metrics = traits::compatible_with<Metric::Minkowski>::value;
+    static constexpr auto engines =
+      arch::traits::pgen::compatible_with<SimEngine::SRPIC>::value;
+    static constexpr auto metrics =
+      arch::traits::pgen::compatible_with<Metric::Minkowski>::value;
     static constexpr auto dimensions =
-      traits::compatible_with<Dim::_1D, Dim::_2D, Dim::_3D>::value;
+      arch::traits::pgen::compatible_with<Dim::_1D, Dim::_2D, Dim::_3D>::value;
 
     // for easy access to variables in the child class
     using arch::ProblemGenerator<S, M>::D;
@@ -79,7 +82,7 @@ namespace user {
     using arch::ProblemGenerator<S, M>::params;
 
     const real_t sx1, sx2, sx3;
-    const real_t vmax;
+    const real_t vmax, temperature;
     const int    nx1, nx2, nx3;
 
     std::vector<real_t> svec;
@@ -93,6 +96,7 @@ namespace user {
               global_domain.mesh().extent(in::x2).first }
       , sx3 { global_domain.mesh().extent(in::x3).second -
               global_domain.mesh().extent(in::x3).first }
+      , temperature { p.get<real_t>("setup.temperature", 0.001) }
       , vmax { p.get<real_t>("setup.vmax", 0.01) }
       , nx1 { p.get<int>("setup.nx1", 10) }
       , nx2 { p.get<int>("setup.nx2", 10) }
@@ -107,15 +111,18 @@ namespace user {
 
     inline void InitPrtls(Domain<S, M>& local_domain) {
       const auto energy_dist = SinEDist<S, M>(local_domain.mesh.metric,
-                                              vmax,
+                                              vmax, temperature,
+                                              local_domain.random_pool(),
                                               nvec,
                                               svec);
-      const auto injector = arch::UniformInjector<S, M, SinEDist>(energy_dist,
-                                                                  { 1, 2 });
-      arch::InjectUniform<S, M, arch::UniformInjector<S, M, SinEDist>>(params,
+      const auto maxwellian   = arch::Maxwellian<S, M>(local_domain.mesh.metric,
+                                                     local_domain.random_pool(),
+                                                     temperature);
+      arch::InjectUniform<S, M, decltype(energy_dist), decltype(maxwellian)>(params,
                                                                        local_domain,
-                                                                       injector,
-                                                                       1.0);
+                                                                       { 1, 2 },
+                                                                       {energy_dist, maxwellian},
+                                                                       ONE);
     }
   };
 
